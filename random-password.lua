fa0057314185320fed6bfabdd49254a42f2309ec
Odanis Esquea <odanisdejesus@gmail.com>
2020-12-18 18:54:39 -0400

    Changed the name of the function from random_password to random_key

--- random-password.lua
+++ random-password.lua
@@ -1,20 +1,20 @@
-local function random_password()
-    -- seed it with the os time with some random ms so we don't get the same char all over again
+local function random_key()
+    -- seed it with the os time and some random ms so we don't get the same char all over again
     math.randomseed(os.time() + math.random(100, 10000))
     local key = math.random(33, 126)
     return utf8.char(key)
 end
 
 local function make_password(tamano_p)
-    if not tamano_p then tamano_p = 10 end 
+    tamano_p = (tamano_p and tamano_p ~= "") or 15
     local password = ""
     for i = 1, tamano_p do
-        password = password .. random_password()
+        password = password .. random_key()
     end
     return password
 end
 
-io.write("size of the password: ")
+io.write("size of the password(default: 15): ")
 local tamano = io.read()
 
 io.write("your password is: " .. make_password(tamano) .. "\n")
